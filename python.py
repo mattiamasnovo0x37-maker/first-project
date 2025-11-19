d4d00856eca83c161a4208e41174cd868ddd4bc2
mattiamasnovo0x37-maker <mattia.masnovo0x37@gmail.com>
2025-11-19 17:55:05 +0100

    Primo esercizio svolto da solo

---
+++ python.py
@@ -0,0 +1,13 @@
+nome = "Mattia"
+print(nome)
+
+età = input("Quanti anni hai?")
+età = int(età)
+età_raddoppiata = età * 2
+print("La tua età raddoppiata è:", età_raddoppiata) 
+
+numero1 = 3
+numero2 = 7
+somma = numero1 + numero2
+print("La somma è:", somma)
+
