ad33427595f9f031b13b0297e80ea9e8758a7316
mattiamasnovo0x37-maker <mattia.masnovo0x37@gmail.com>
2025-11-21 17:38:12 +0100

    seconda parte di esercizi

--- esercizio2casting,datatypes,variabili.py
+++ esercizio2casting,datatypes,variabili.py
@@ -18,3 +18,43 @@ somma = x + y + z
 print(int(somma))
 print(type(somma))
 
+#Esercizio4: lo scambio di valori
+x = 20
+y = 15
+x,y = y,x
+print(x,y)
+
+#Esercizio5: calcolo dell'aria di un rettangolo
+base = 5
+altezza = 10
+area = base * altezza
+print("area rettangolo:", area)
+
+#Esercizio6: somma di un numero intero con un decimale
+a = 10
+b = 3.5
+print(a + b)
+#print(type(a, b, a + b)) --> versione sbagliata
+print(type(a), type(b), type(a + b)) #Giusta, corretta con ChatGPT
+
+#Esercizio7: calcolo della media di tre dati
+x, y, z = 10, 40, 30
+media = (x + y + z)/3
+print("media:", media)
+
+#Esercizio8: concatenazione di stringhe
+s1 = "mi piace"
+s2 = " giocare a calcio"
+s3 = " per fama, soldi e figa"
+print(s1 + s2 + s3)
+
+#Esercizio9: ripetizione di tre stringhe
+print("ciao " *3)
+
+#Esercizio10: usare i boolean per verificare se un numero è maggiore di un altro
+a =  15
+b = 30
+c = 45
+print(a < b)
+print(b > c)
+print(a < c)
