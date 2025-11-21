9213ea25469ffca591ae19e7af43b87dd6546bcd
mattiamasnovo0x37-maker <mattia.masnovo0x37@gmail.com>
2025-11-21 17:05:10 +0100

    esercizi svolti durante la lezione 1.0

---
+++ esercizio2casting,datatypes,variabili.py
@@ -0,0 +1,20 @@
+# Esercizio1
+nome = "Mattia"
+età = 22
+città = "Bolzano"
+print(nome, età, città)
+
+#Esercizio2: creare una variabile "X", darle un valore e aggiornarla con un nuovo valore
+x = 5
+print(x)
+x = 10
+print(x)
+
+#Esercizio3: creare tre variabili intere + la loro somma
+x = 17
+y = 37
+z = 54
+somma = x + y + z
+print(int(somma))
+print(type(somma))
+
